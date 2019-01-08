fcce587bee12e7d931c2a67e07b53fd1504ac8b6
Leo <a0916364056@gmail.com>
2019-01-08 17:29:02 +0800

    優化輸入驗證碼體驗
    使User 按tab鍵切換input順暢

--- Vue_SPA/src/components/login_Signup_Message.vue
+++ Vue_SPA/src/components/login_Signup_Message.vue
@@ -7,15 +7,50 @@
           <br>並輸入驗證碼！
         </div>
         <div class="validation-code" v-if="showvalidation_message">
-          <input type="text" id="first-validation-code" ref="fvc" v-focus="showvalidation_message">
+          <input
+            type="text"
+            id="first_validation_code"
+            ref="firstvc"
+            v-focus="showvalidation_message"
+            :style="com_focus_first"
+            v-on:keydown.9="nextfocus('svc')"
+            :tabindex="tabindex1"
+          >
           -
-          <input type="text" id="second-validation-code" ref="svc">
+          <input
+            type="text"
+            id="second_validation_code"
+            ref="svc"
+            v-on:keydown.9="nextfocus('tvc')"
+            :style="com_focus_second"
+            :tabindex="tabindex2"
+          >
           -
-          <input type="text" id="third-validation-code" ref="tvc">
+          <input
+            type="text"
+            id="third_validation_code"
+            ref="tvc"
+            v-on:keydown.9="nextfocus('fvc')"
+            :style="com_focus_third"
+            :tabindex="tabindex3"
+          >
           -
-          <input type="text" id="fourth-validation-code" ref="fvc">
+          <input
+            type="text"
+            id="fourth_validation_code"
+            ref="fvc"
+            :style="com_focus_fourth"
+            v-on:keydown.9="nextfocus('click')"
+            :tabindex="tabindex4"
+          >
         </div>
-        <button class="modal-default-button" @click="emitparent()">OK</button>
+        <button
+          class="modal-default-button"
+          ref="click"
+          @click="emitparent()"
+          v-on:keydown.9="nextfocus('firstvc')"
+          :tabindex="tabindex5"
+        >OK</button>
       </div>
     </div>
   </div>
@@ -24,20 +59,77 @@
 <script>
 export default {
   props: ["Response_Message_s", "showvalidation_message"],
+  data() {
+    return {
+      focus_first_color: "#f5548c",
+      focus_second_color: "rgb(56, 180, 76)",
+      focus_third_color: "rgb(56, 180, 76)",
+      focus_fourth_color: "rgb(56, 180, 76)",
+      tabindex1: 1,
+      tabindex2: 2,
+      tabindex3: 3,
+      tabindex4: 4,
+      tabindex5: 5
+    };
+  },
   methods: {
     emitparent: function() {
       this.$emit("hiddenMessage", false);
+    },
+    nextfocus: function(ref) {
+      if (ref == "svc") {
+        this.focus_first_color = "rgb(56, 180, 76)";
+        this.focus_second_color = "#f5548c";
+        this.tabindex1 += 5;
+      } else if (ref == "tvc") {
+        this.focus_second_color = "rgb(56, 180, 76)";
+        this.focus_third_color = "#f5548c";
+        this.tabindex2 += 5;
+      } else if (ref == "fvc") {
+        this.focus_third_color = "rgb(56, 180, 76)";
+        this.focus_fourth_color = "#f5548c";
+        this.tabindex3 += 5;
+      } else if (ref == "click") {
+        this.focus_first_color = "rgb(56, 180, 76)";
+        this.focus_second_color = "rgb(56, 180, 76)";
+        this.focus_third_color = "rgb(56, 180, 76)";
+        this.focus_fourth_color = "rgb(56, 180, 76)";
+        this.tabindex4 += 5;
+      } else {
+        this.focus_first_color = "#f5548c";
+        this.tabindex5 += 5;
+        if (this.tabindex5 == 10) {
+          this.tabindex1 = 1;
+          this.tabindex2 = 2;
+          this.tabindex3 = 3;
+          this.tabindex4 = 4;
+          this.tabindex5 = 5;
+        }
+      }
+    }
+  },
+  computed: {
+    com_focus_first() {
+      return { border: "2px " + this.focus_first_color + " solid" };
+    },
+    com_focus_second() {
+      return { border: "2px " + this.focus_second_color + " solid" };
+    },
+    com_focus_third() {
+      return { border: "2px " + this.focus_third_color + " solid" };
+    },
+    com_focus_fourth() {
+      return { border: "2px " + this.focus_fourth_color + " solid" };
     }
   },
   directives: {
     focus: {
-     inserted: function (el) {
+      inserted: function(el) {
         // 聚焦元素
-        el.focus()
-        }
-      
+        el.focus();
+      }
     }
-  },
+  }
 };
 </script>
 
